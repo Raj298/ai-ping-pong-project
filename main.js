2f766d8b4a51076c846994de0667eabf754dc1db
Raj298 <71762923+Raj298@users.noreply.github.com>
2021-03-31 20:15:13 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+
+
+
+
+function preload() {
+
+}
+
+function setup() {
+	video=createCapture(VIDEO)
+	video.size(800,400);
+	video.parent('game_console')
+	
+	
+}
+
+function draw() {
+
+}
+
+
+
+
